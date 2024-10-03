9dfd671f9624e83066e7f8850a9224862d2cc1eb
Nobert-Muma <mumanobert17@gmail.com>
2024-10-03 15:13:35 +0300

    Flask-SQLALchemy Lab1

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -22,6 +22,47 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def get_by_id(id):
+    earthquake=Earthquake.query.filter(Earthquake.id==id).first()
+    if earthquake:
+        body={
+            'id':earthquake.id,
+            'location':earthquake.location,
+            'magnitude':earthquake.magnitude,
+            'year':earthquake.year
+        }
+        status_code=200
+        headers={}
+    else:
+        body={
+            'message':'Earthquake 9999 not found.'
+        }
+        status_code=404
+        headers={}
+    return make_response(body, status_code, headers)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def find_magnitude(magnitude):
+    earthquakess=Earthquake.query.filter(Earthquake.magnitude>=magnitude).all()
+    earthquakes=[]
+    if earthquakess:
+        for earthquake in earthquakess:
+            earthquakes.append(earthquake)
+        response_body={
+            'count':len(earthquakes),
+            'quakes':earthquakes
+        }
+        status_code=200
+        headers={}
+    else:
+        response_body={
+            'count':0,
+            'quakes':[]
+        }
+        status_code=200
+        headers={}
+    return make_response(response_body, status_code, headers)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
